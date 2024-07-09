8f0502a2f8d121fe9312a3b1522a350b7eed91ae
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2024-07-09 06:10:20 +0600

    first commit

--- src/app/utils/sendImageToCloudinary.ts
+++ src/app/utils/sendImageToCloudinary.ts
@@ -1,7 +1,7 @@
-import { UploadApiResponse, v2 as cloudinary } from 'cloudinary';
-import fs from 'fs';
-import multer from 'multer';
-import config from '../../config';
+import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import multer from "multer";
+import config from "../config";
 
 // Todo this file is responsible for custom made function to upload image to cloudinary, receiving file using multer and deleting file from temporary folder. Read the following blog to get a comprehensive understanding https://dev.to/md_enayeturrahman_2560e3/how-to-save-image-with-multer-j74
 
@@ -13,7 +13,7 @@ cloudinary.config({
 
 export const sendImageToCloudinary = (
   imageName: string,
-  path: string,
+  path: string
 ): Promise<Record<string, unknown>> => {
   return new Promise((resolve, reject) => {
     cloudinary.uploader.upload(
@@ -29,21 +29,21 @@ export const sendImageToCloudinary = (
           if (err) {
             console.log(err);
           } else {
-            console.log('File is deleted.');
+            console.log("File is deleted.");
           }
         });
-      },
+      }
     );
   });
 };
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, process.cwd() + '/uploads/');
+    cb(null, process.cwd() + "/uploads/");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
   },
 });
 
