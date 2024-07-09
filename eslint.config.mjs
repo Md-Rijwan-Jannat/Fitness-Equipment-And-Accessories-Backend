1fe3c1d2a423a6a1df4f0f9edaddf0ecfbc6f539
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2024-07-09 06:19:48 +0600

    .env file update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,25 @@
-import eslint from "@eslint/js"
-import tseslint from "@typescript-eslint"
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint";
 
 export default tseslint.config(
-  eslint.configs.recommended, 
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
     languageOptions: {
       globals: {
-...globals.node,
-      }
-    }
+        ...globals.node,
+      },
+    },
   },
   {
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
-      "prefer-const" : "error",
+      "prefer-const": "error",
       "no-console": "warn",
-    }
+    },
   },
   {
-    ignores: ["**/dist/", "**/node_modules/"]
+    ignores: ["**/dist/", "**/node_modules/"],
   }
-)
+);
